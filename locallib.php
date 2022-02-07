dc5830a20805a1d8cc85810c4b94b3c6f60bb829
Tuomas Alanen <tuomas.alanen@helsinki.fi>
2022-02-07 13:13:47 +0200

    code style fixes

--- locallib.php
+++ locallib.php
@@ -22,7 +22,7 @@
  * @license     https://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
  */
 
-defined('MOODLE_INTERNAL') || die();
+
 
 /**
  * Used to generate page urls for digitala module.
@@ -32,49 +32,49 @@ defined('MOODLE_INTERNAL') || die();
  * @param number $d id of the activity instance
  */
 function page_url($page, $id, $d) {
-	return new moodle_url('/mod/digitala/view.php', array('id' => $id, 'd' => $d, 'page' => $page));
+    return new moodle_url('/mod/digitala/view.php', array('id' => $id, 'd' => $d, 'page' => $page));
 }
 
 /**
  * Used to generate links in the steps of the progress bar.
- * 
+ *
  * @param string $name name of the step
  * @param number $page number of the step
  * @param number $id id of the course module
  * @param number $d id of the activity instance
- * @param bool $is_curr true if page is currently active
+ * @param bool $isCurr true if page is currently active
  */
-function create_progress_bar_step_link($name, $page, $id, $d, $is_curr) {
-	$url = page_url($page, $id, $d);
-	$page_out = $page + 1;
-	if ($is_curr) {
-		$title = '<span class="pb-num active">'.$page_out.'</span>'.$name;
-	} else {
-		$title = '<span class="pb-num">'.$page_out.'</span>'.$name;
-	}
-	$out = html_writer::link($url, $title, array('class' => 'display-6'));
-	return $out;
+function create_progress_bar_step_link($name, $page, $id, $d, $isCurr) {
+    $url = page_url($page, $id, $d);
+    $page_out = $page + 1;
+    if ($isCurr) {
+        $title = '<span class="pb-num active">'.$page_out.'</span>'.$name;
+    } else {
+        $title = '<span class="pb-num">'.$page_out.'</span>'.$name;
+    }
+    $out = html_writer::link($url, $title, array('class' => 'display-6'));
+    return $out;
 }
 
 /**
  * Used to begin creation of the progress bar.
  */
 function start_progress_bar() {
-	$out = html_writer::start_div('digitala-progress-bar');
-	return $out;
+    $out = html_writer::start_div('digitala-progress-bar');
+    return $out;
 }
 
 /**
  * Used to end creation of the progress bar.
  */
 function end_progress_bar() {
-	$out = html_writer::end_div();
-	return $out;
+    $out = html_writer::end_div();
+    return $out;
 }
 
 /**
  * Used to create one step of the progress bar.
- * 
+ *
  * @param string $name name of the step
  * @param number $page number of the step
  * @param number $id id of the course module
@@ -82,57 +82,57 @@ function end_progress_bar() {
  * @param number $curr_page number of the active page
  */
 function create_progress_bar_step($name, $page, $id, $d, $curr_page) {
-	$classes = 'pb-step';
-	$is_curr = $page == $curr_page;
-	if ($is_curr) {
-		$classes .= ' active';
-	}
-	if ($page == 0) {
-		$classes .= ' first';
-	}
-	if ($page == 2) {
-		$classes .= ' last';
-	}
-	
-	$out = html_writer::start_div($classes);
-	$out .= create_progress_bar_step_link($name, $page, $id, $d, $is_curr);
-	$out .= html_writer::end_div();
-	return $out;
+    $classes = 'pb-step';
+    $is_curr = $page == $curr_page;
+    if ($is_curr) {
+        $classes .= ' active';
+    }
+    if ($page == 0) {
+        $classes .= ' first';
+    }
+    if ($page == 2) {
+        $classes .= ' last';
+    }
+
+    $out = html_writer::start_div($classes);
+    $out .= create_progress_bar_step_link($name, $page, $id, $d, $is_curr);
+    $out .= html_writer::end_div();
+    return $out;
 }
 
 /**
  * Helper function that is used to calculate if highlight color is needed in the spacer.
- * 
+ *
  * @param number $page number of the page
  */
 function calculate_progress_bar_spacers($page) {
-	if ($page == 0) {
-		return array('left' => 'right-empty', 'right' => 'nothing');
-	} elseif ($page == 1) {
-		return array('left' => 'left-empty', 'right' => 'right-empty');
-	} else {
-		return array('left' => 'nothing', 'right' => 'left-empty');
-	}
+    if ($page == 0) {
+        return array('left' => 'right-empty', 'right' => 'nothing');
+    } elseif ($page == 1) {
+        return array('left' => 'left-empty', 'right' => 'right-empty');
+    } else {
+        return array('left' => 'nothing', 'right' => 'left-empty');
+    }
 }
 
 /** 
  * Used to create spacer between steps in the progress bar.
- * 
+ *
  * @param string $mode defines if extra filling needed in the spacer. Knows values 'right-empty' and 'left-empty'. Other strings gives no extra filling. 
 */
 function create_progress_bar_spacer($mode) {
-	$out = html_writer::start_div('pb-spacer');
-	$out .= '<svg width="100%" height="100%" viewBox="0 0 275 500" style="fill-rule:evenodd;clip-rule:evenodd;stroke-linecap:round;stroke-linejoin:round;stroke-miterlimit:1.5;">';
-	
-	if ($mode == 'left-empty') {
-		$out .= '<path d="M275,0L20,0L255,250L20,500L275,500L275,0Z" style="fill:rgb(211,211,211);"/>';
-	}	
-	
-	if ($mode == 'right-empty') {
-		$out .= '<path d="M255,250L20,0L0,0L0,500L20,500L255,250Z" style="fill:rgb(211,211,211);"/>';
-	}
-	$out .= '<path d="M20,20L255,250L20,480" style="fill:none;stroke:rgb(211,211,211);stroke-width:40px;"/>';
-	$out .= '</svg>';
-	$out .= html_writer::end_div();
-	return $out;
+    $out = html_writer::start_div('pb-spacer');
+    $out .= '<svg width="100%" height="100%" viewBox="0 0 275 500" style="fill-rule:evenodd;clip-rule:evenodd;stroke-linecap:round;stroke-linejoin:round;stroke-miterlimit:1.5;">';
+
+    if ($mode == 'left-empty') {
+        $out .= '<path d="M275,0L20,0L255,250L20,500L275,500L275,0Z" style="fill:rgb(211,211,211);"/>';
+    }	
+
+    if ($mode == 'right-empty') {
+        $out .= '<path d="M255,250L20,0L0,0L0,500L20,500L255,250Z" style="fill:rgb(211,211,211);"/>';
+    }
+    $out .= '<path d="M20,20L255,250L20,480" style="fill:none;stroke:rgb(211,211,211);stroke-width:40px;"/>';
+    $out .= '</svg>';
+    $out .= html_writer::end_div();
+    return $out;
 }
