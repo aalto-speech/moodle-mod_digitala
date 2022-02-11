af50508aabf5a89a0f343513478f7a22a63f7de7
Maikki Heijala <maikki.heijala@live.com>
2022-02-11 18:35:34 +0200

    unit test for helper functions and base for assignment test

---
+++ tests/view_helper_functions_test.php
@@ -0,0 +1,53 @@
+<?php
+// This file is part of Moodle - http://moodle.org/
+//
+// Moodle is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Moodle is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
+
+//namespace mod_digitala;
+
+defined('MOODLE_INTERNAL') || die('Direct Access is forbidden!');
+
+global $CFG;
+require_once($CFG->dirroot . '/mod/digitala/locallib.php');
+
+/**
+ * Unit tests for view creation helpers: container, card and column. 
+ *
+ * @package     mod_digitala
+ * @category    test
+ * @copyright   2022 Name
+ * @license     https://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ */
+class view_helper_functions_test extends \advanced_testcase {
+    
+    /**
+     * Test creating new assignment object.
+     */
+    public function test_container_html_output_right() {
+        $result = start_container('some_step_of_digitala');
+        $result .= end_container();    
+        $this->assertEquals('<div class="some_step_of_digitala"><div class="container-fluid"><div class="row"></div></div></div>', $result);
+    }
+
+    public function test_column_html_output_right() {
+        $result = start_column();
+        $result .= end_column();    
+        $this->assertEquals('<div class="col digitala-column"></div>', $result);
+    }    
+    
+    public function test_card_html_output_right() {
+        $result = create_card('pluginname', 'Some text here');   
+        $this->assertEquals('<div class="card row"><div class="card-body"><h5 class="card-title">Digitala</h5><div class="card-body">Some text here</div></div></div>', $result);
+    }
+}  
