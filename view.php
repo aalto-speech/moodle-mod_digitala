db19a2a8772c2a2f47c7c94a0ca97e53e40cb35c
Maikki Heijala <maikki.heijala@live.com>
2022-02-15 21:45:29 +0200

    transcript text with scrollbar

--- view.php
+++ view.php
@@ -77,7 +77,7 @@ $resourcetext = '<iframe width="100%" height="500" src="https://www.youtube.com/
 // Temporary output of the report we receive. Format could/will change as we do not yet have access to the server.
 $reportoutput = '{
     "transcription": [
-        {"transtext": "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."}
+        {"transtext": "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum. Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."}
     ], 
     "grades": [
         {"name": "Completing the task", "grade": 3, "maxgrade": 3, "reporttext": "No significant shortcomings."},
