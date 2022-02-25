ef6fa29cf02146d30615abbc980b24225f0c86a6
Tuomas Alanen <tualanen@cs.helsinki.fi>
2022-02-25 13:30:52 +0200

    work dir should be right

--- scripts/bump-version.sh
+++ scripts/bump-version.sh
@@ -1,20 +1,9 @@
 #!/bin/bash
-echo $(ls)
-cd plugin
-echo $(ls)
 old_version=$(cat version.php | grep '$plugin->version = ')
 old_version=${old_version/\$plugin->version = /}
 old_version=${old_version/;/}
 
 new_version=$((old_version + 1))
-
-if [ $? -eq 0]
-then
-    echo 'Found current version: ' ${old_version}
-else
-    echo 'version.php not found'
-    exit 1
-fi
 
 sed -i "s+${old_version}+${new_version}+g" version.php
 
