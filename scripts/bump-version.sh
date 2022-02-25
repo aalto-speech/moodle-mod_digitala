a4588326888ddd361265ee41aa1dcfb0f2bdf464
Tuomas Alanen <tualanen@cs.helsinki.fi>
2022-02-25 12:33:25 +0200

    add bump-version.sh

---
+++ scripts/bump-version.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+old_version=$(cat version.php | grep '$plugin->version = ')
+old_version=${old_version/\$plugin->version = /}
+old_version=${old_version/;/}
+
+new_version=$((old_version + 1))
+
+sed -i "s+${old_version}+${new_version}+g" version.php
+
+echo 'Updated plugin version from '${old_version}' to '${new_version}
