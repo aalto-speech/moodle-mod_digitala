cc4606b421fc461595f56c4d198445bd089476a1
Tuomas Alanen <tualanen@cs.helsinki.fi>
2022-03-16 11:36:09 +0200

    add dummy fin-savo lang file for testing course lang

---
+++ lang/fi/digitala.php
@@ -0,0 +1,57 @@
+<?php
+// This file is part of Moodle - https://moodle.org/
+//
+// Moodle is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Moodle is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Moodle.  If not, see <https://www.gnu.org/licenses/>.
+
+/**
+ * Plugin strings are defined here.
+ *
+ * @package     mod_digitala
+ * @category    string
+ * @copyright   2022 Name
+ * @license     https://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ */
+
+defined('MOODLE_INTERNAL') || die();
+
+$string['pluginname'] = 'Digitala';
+$string['modulename'] = 'Digitala';
+$string['digitala:addinstance'] = 'Lisää uusi Digitala harjoite';
+$string['modulenameplural'] = 'Digitaloja';
+$string['digitalaname'] = 'Harjoitteen nimi';
+$string['digitalaname_help'] = 'Harjoitteen nimi, eikös juu?';
+$string['digitalaattemptlang'] = 'Harjoitteen kieli';
+$string['digitalaattemptlang_help'] = 'Kieli, jossa opiskelija vastaa tehtävään.';
+$string['digitalafinnish'] = 'Suomi';
+$string['digitalaswedish'] = 'Ruotsi';
+$string['digitalaattempttype'] = 'Harjoitteen tyyppi';
+$string['digitalaattempttype_help'] = 'töttöröö';
+$string['digitalareadaloud'] = 'Huastele vappaasti';
+$string['digitalafreeform'] = 'Vapaa sana';
+$string['pluginadministration'] = 'Laajennoksen hallinnointi';
+$string['digitalainfo'] = 'Tietoa';
+$string['digitalainfotext'] = 'Testooppa tuota mikrofoonias että se suattaapi toimia seuraavassa vaeheessa.';
+$string['digitalaassignment'] = 'Harjoite';
+$string['digitalaassignment_help'] = 'Harjoite, jonka opiskelija suorittaapi.';
+$string['digitalaassignmentresource'] = 'Resurssit';
+$string['digitalaassignmentresource_help'] = 'Resurssit, jotka opiskelija näkkeepi tehdessään tehtävöö';
+$string['digitalareport'] = 'Rapsa';
+$string['digitalareportnotavailable'] = 'Ei oo rapsoo näkyny.';
+$string['digitalatranscription'] = 'Mittee nää sanoit';
+$string['digitalaassignmentrecord'] = 'Nauhoitappa vastaukses';
+$string['digitalasubmitanswer'] = 'Lähetäppä vastaukses';
+$string['digitalanavnext'] = 'Seuraava vaehe >';
+$string['digitalanavprevious'] = '< Edellinen vaehe';
+$string['digitalanavstartagain'] = 'Alotappa alusta';
+$string['digitalanavfeedback'] = 'Anna palautetta';
