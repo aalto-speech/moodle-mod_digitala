727de88834e41a8d6107b2f67b2803ddadd05a31
Maikki Heijala <maikki.heijala@live.com>
2022-03-18 10:33:08 +0200

    Style fixes
    Co-authored-by: Tuomas <tualanen@cs.helsinki.fi>

--- mod_form.php
+++ mod_form.php
@@ -76,16 +76,18 @@ class mod_digitala_mod_form extends moodleform_mod {
         $mform->addHelpButton('attempttype', 'digitalaattempttype', 'mod_digitala');
 
         // Adding the "assignment" field.
-        //$mform->addElement('editor', 'assignment', get_string("digitalaassignment", "mod_digitala"), array('rows' => 10));
-        $mform->addElement('textarea', 'assignment', get_string("digitalaassignment", "mod_digitala"), array('rows' => 10, 'cols' => '64'));
+        $mform->addElement('textarea', 'assignment', get_string("digitalaassignment", "mod_digitala"),
+                           array('rows' => 10, 'cols' => '64'));
 
         $mform->setType('assignment', PARAM_TEXT);
         $mform->addRule('assignment', null, 'required', null, 'client');
         $mform->addHelpButton('assignment', 'digitalaassignment', 'mod_digitala');
 
         // Adding the "resources" field.
-        $textfieldoptions = array('trusttext' => false, 'subdirs' => true, 'maxfiles' => -1, 'maxbytes' => $CFG->maxbytes, 'context' => $this->context, 'changeformat' => true, 'noclean' => true);
-        $mform->addElement('editor', 'resources', get_string("digitalaassignmentresource", "mod_digitala"), array('rows' => 10), $textfieldoptions);
+        $textfieldoptions = array('trusttext' => false, 'subdirs' => true, 'maxfiles' => -1, 'maxbytes' => $CFG->maxbytes,
+                                  'context' => $this->context, 'changeformat' => true, 'noclean' => true);
+        $mform->addElement('editor', 'resources', get_string("digitalaassignmentresource", "mod_digitala"),
+                           array('rows' => 10), $textfieldoptions);
 
         $mform->setType('resources', PARAM_RAW);
         $mform->addHelpButton('resources', 'digitalaassignmentresource', 'mod_digitala');
