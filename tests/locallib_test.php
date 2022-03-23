f84092c8c8dbd82dae49bdf753c2b493174ceea1
Tuomas Alanen <tualanen@cs.helsinki.fi>
2022-03-23 16:09:38 +0200

    fixed namespace errors

--- tests/locallib_test.php
+++ tests/locallib_test.php
@@ -297,10 +297,10 @@ class locallib_test extends \advanced_testcase {
         global $USER;
 
         $context = \context_module::instance($this->digitala->cmid);
-        $assignment = new digitala_assignment($this->digitala->id, $context->id, $USER->id, $USER->username, 1, 1, $this->digitala->assignment, $this->digitala->resources, $this->digitala->attempttype, $this->digitala->attemptlang);  // phpcs:ignore moodle.Files.LineLength.MaxExceeded
+        $assignment = new \digitala_assignment($this->digitala->id, $context->id, $USER->id, $USER->username, 1, 1, $this->digitala->assignment, $this->digitala->resources, $this->digitala->attempttype, $this->digitala->attemptlang);  // phpcs:ignore moodle.Files.LineLength.MaxExceeded
 
         $fileinfo = array(
-            'contextid' => context_user::instance($USER->id)->id,
+            'contextid' => \context_user::instance($USER->id)->id,
             'component' => 'user',
             'filearea' => 'draft',
             'itemid' => 0,
@@ -313,7 +313,7 @@ class locallib_test extends \advanced_testcase {
         $file = $fs->get_file($fileinfo['contextid'], $fileinfo['component'], $fileinfo['filearea'],
                           $fileinfo['itemid'], $fileinfo['filepath'], $fileinfo['filename']);
 
-        $formdata = new stdClass();
+        $formdata = new \stdClass();
         $formdata->audiostring = '{"url":"http:\/\/localhost:8000\/draftfile.php\/5\/user\/draft\/0\/testing.wav","id": 0,"file":"testing.wav"}'; // phpcs:ignore moodle.Files.LineLength.MaxExceeded
         $result = save_answerrecording($formdata, $assignment);
         $this->assertEquals('accessed without internet successful', $result);
@@ -326,7 +326,7 @@ class locallib_test extends \advanced_testcase {
         global $USER;
 
         $context = \context_module::instance($this->digitala->cmid);
-        $assignment = new digitala_assignment($this->digitala->id, $context->id, $USER->id, $USER->username, 4, 5, $this->digitala->assignment, $this->digitala->resources, $this->digitala->attempttype, $this->digitala->attemptlang); // phpcs:ignore moodle.Files.LineLength.MaxExceeded
+        $assignment = new \digitala_assignment($this->digitala->id, $context->id, $USER->id, $USER->username, 4, 5, $this->digitala->assignment, $this->digitala->resources, $this->digitala->attempttype, $this->digitala->attemptlang); // phpcs:ignore moodle.Files.LineLength.MaxExceeded
 
         $result = create_answerrecording_form($assignment);
         if (PHPUnitVersion::series() < 9) {
@@ -350,9 +350,9 @@ class locallib_test extends \advanced_testcase {
      */
     public function test_create_answerrecording_form_with_data() {
         global $USER;
-        answerrecording_form::mock_submit(array('audiostring' => '{"url":"http:\/\/localhost:8000\/draftfile.php\/5\/user\/draft\/0\/testing.wav","id": 0,"file":"testing.wav"}'), null, 'post', 'answerrecording_form'); // phpcs:ignore moodle.Files.LineLength.MaxExceeded
+        \answerrecording_form::mock_submit(array('audiostring' => '{"url":"http:\/\/localhost:8000\/draftfile.php\/5\/user\/draft\/0\/testing.wav","id": 0,"file":"testing.wav"}'), null, 'post', 'answerrecording_form'); // phpcs:ignore moodle.Files.LineLength.MaxExceeded
         $context = \context_module::instance($this->digitala->cmid);
-        $assignment = new digitala_assignment($this->digitala->id, $context->id, $USER->id, $USER->username, 1, 1, $this->digitala->assignment, $this->digitala->resources, $this->digitala->attempttype, $this->digitala->attemptlang); // phpcs:ignore moodle.Files.LineLength.MaxExceeded
+        $assignment = new \digitala_assignment($this->digitala->id, $context->id, $USER->id, $USER->username, 1, 1, $this->digitala->assignment, $this->digitala->resources, $this->digitala->attempttype, $this->digitala->attemptlang); // phpcs:ignore moodle.Files.LineLength.MaxExceeded
 
         $result = create_answerrecording_form($assignment);
         $this->assertEquals('No evaluation was found. Please return to previous page.', $result);
