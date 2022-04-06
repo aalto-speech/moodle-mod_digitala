c15873238a55885c9ae79aafcf97cb557738d920
Tuomas Alanen <tualanen@cs.helsinki.fi>
2022-04-06 23:42:03 +0300

    second check everything is okay

--- version.php
+++ version.php
@@ -25,7 +25,7 @@
 defined('MOODLE_INTERNAL') || die();
 
 $plugin->component = 'mod_digitala';
-$plugin->release = '0.1.17';
-$plugin->version = 2022040617;
+$plugin->release = '0.1.0';
+$plugin->version = 2022040600;
 $plugin->requires = 2020061500;
 $plugin->maturity = MATURITY_ALPHA;
