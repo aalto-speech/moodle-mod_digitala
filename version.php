09faa57a3209dfea624803fabd3bb79868c16815
Tuomas Alanen <FinThunderstorm@users.noreply.github.com>
2022-04-08 13:33:23 +0000

    Bump version & release

--- version.php
+++ version.php
@@ -25,7 +25,7 @@
 defined('MOODLE_INTERNAL') || die();
 
 $plugin->component = 'mod_digitala';
-$plugin->release = '0.1.2';
-$plugin->version = 2022040800;
+$plugin->release = '0.1.3';
+$plugin->version = 2022040801;
 $plugin->requires = 2020061500;
 $plugin->maturity = MATURITY_ALPHA;
