b9d0f91af67aec4d23eeeadc16c05e2cc7079a40
Tuomas Alanen <tualanen@cs.helsinki.fi>
2022-04-20 13:27:02 +0300

    bug fixes

--- locallib.php
+++ locallib.php
@@ -645,27 +645,27 @@ function save_attempt($assignment, $filename, $evaluation, $recordinglength) {
     $attempt->file = $filename;
     $attempt->transcript = $evaluation->transcript;
     if ($assignment->attempttype == 'freeform') {
-        $attempt->taskcompletion = $attempt->taskcompletion > 3 ? 0 : $evaluation->task_completion;
+        $attempt->taskcompletion = $evaluation->task_completion > 3 ? 0 : $evaluation->task_completion;
         $attempt->taskcompletion = $attempt->taskcompletion < 0 ? 0 : $attempt->taskcompletion;
         $attempt->taskcompletion = round($attempt->taskcompletion, 0, 2);
-        $attempt->fluency = $attempt->fluency > 4 ? 0 : $evaluation->fluency->score;
+        $attempt->fluency = $evaluation->fluency->score > 4 ? 0 : $evaluation->fluency->score;
         $attempt->fluency = $attempt->fluency < 0 ? 0 : $attempt->fluency;
         $attempt->fluency = round($attempt->fluency, 2);
         $attempt->fluency_features = json_encode($evaluation->fluency->flu_features);
-        $attempt->pronunciation = $attempt->pronunciation > 4 ? 0 : $evaluation->pronunciation->score;
+        $attempt->pronunciation = $evaluation->pronunciation->score > 4 ? 0 : $evaluation->pronunciation->score;
         $attempt->pronunciation = $attempt->pronunciation < 0 ? 0 : $attempt->pronunciation;
         $attempt->pronunciation = round($attempt->pronunciation, 2);
         $attempt->pronunciation_features = json_encode($evaluation->pronunciation->pron_features);
-        $attempt->lexicogrammatical = $attempt->lexicogrammatical > 3 ? 0 : $evaluation->lexicogrammatical->score;
+        $attempt->lexicogrammatical = $evaluation->lexicogrammatical->score > 3 ? 0 : $evaluation->lexicogrammatical->score;
         $attempt->lexicogrammatical = $attempt->lexicogrammatical < 0 ? 0 : $attempt->lexicogrammatical;
         $attempt->lexicogrammatical = round($attempt->lexicogrammatical, 2);
         $attempt->lexicogrammatical_features = json_encode($evaluation->lexicogrammatical->lexgram_features);
-        $attempt->holistic = $attempt->holistic > 6 ? 0 : $evaluation->holistic;
+        $attempt->holistic = $evaluation->holistic > 6 ? 0 : $evaluation->holistic;
         $attempt->holistic = $attempt->holistic < 0 ? 0 : $attempt->holistic;
         $attempt->holistic = round($attempt->holistic, 2);
     } else {
         $attempt->feedback = $evaluation->feedback;
-        $attempt->gop_score = $attempt->gop_score < 1 ? 0 : $evaluation->GOP_score;
+        $attempt->gop_score = $evaluation->GOP_score < 1 ? 0 : $evaluation->GOP_score;
         $attempt->gop_score = $attempt->gop_score > 0 ? 0 : $attempt->gop_score;
         $attempt->gop_score = round($attempt->gop_score, 2);
     }
