428890433cd953f1febb4967780e5ec16bde9deb
jonthus <jonthus@users.noreply.github.com>
2022-04-22 17:49:38 +0000

    Bump version & release

--- version.php
+++ version.php
@@ -25,7 +25,7 @@
 defined('MOODLE_INTERNAL') || die();
 
 $plugin->component = 'mod_digitala';
-$plugin->release = '0.1.14';
-$plugin->version = 2022042100;
+$plugin->release = '0.1.15';
+$plugin->version = 2022042200;
 $plugin->requires = 2020061500;
 $plugin->maturity = MATURITY_ALPHA;
