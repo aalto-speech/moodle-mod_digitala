568f5668901c16556a3748c998e22c58be00c239
jonthus <joona.erkkila1@gmail.com>
2022-04-23 22:11:04 +0300

    Style fixes

--- mod_form.php
+++ mod_form.php
@@ -80,7 +80,7 @@ class mod_digitala_mod_form extends moodleform_mod {
         $mform->addHelpButton('attempttype', 'attempttype', 'mod_digitala');
 
         // Adding the "maxlength" field for assignment timelimit.
-        $mform->addElement('duration', 'maxlength', get_string('timelimit', 'mod_digitala'), 
+        $mform->addElement('duration', 'maxlength', get_string('timelimit', 'mod_digitala'),
                 array('optional' => false, 'units' => array(1, MINSECS)));
 
         // Adding the "attemptnumber" field.
