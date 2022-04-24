ce2307166d9733d064338f7ff63d343808cb4a65
Maikki Heijala <makitzei@users.noreply.github.com>
2022-04-24 12:02:33 +0000

    Bump version & release

--- version.php
+++ version.php
@@ -25,7 +25,7 @@
 defined('MOODLE_INTERNAL') || die();
 
 $plugin->component = 'mod_digitala';
-$plugin->release = '0.1.18';
-$plugin->version = 2022042400;
+$plugin->release = '0.1.19';
+$plugin->version = 2022042401;
 $plugin->requires = 2020061500;
 $plugin->maturity = MATURITY_ALPHA;
