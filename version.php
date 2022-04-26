307900fbb4e0f987cb83ed978b7b787c0fc2003a
Topi <topi@harjunpaa.fi>
2022-04-26 09:02:46 +0000

    Bump version & release

--- version.php
+++ version.php
@@ -25,7 +25,7 @@
 defined('MOODLE_INTERNAL') || die();
 
 $plugin->component = 'mod_digitala';
-$plugin->release = '0.1.22';
-$plugin->version = 2022042405;
+$plugin->release = '0.1.23';
+$plugin->version = 2022042600;
 $plugin->requires = 2020061500;
 $plugin->maturity = MATURITY_ALPHA;
