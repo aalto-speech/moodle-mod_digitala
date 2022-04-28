fe636e76ce0bd4fcb49c5f9dd6442776e7d12f3f
Maikki Heijala <maikki.heijala@live.com>
2022-04-28 11:28:23 +0300

    Change order of table titles too

--- renderer.php
+++ renderer.php
@@ -224,8 +224,8 @@ class mod_digitala_renderer extends plugin_renderer_base {
                 new html_table_cell(get_string('results_time', 'digitala')),
                 new html_table_cell(get_string('results_tries', 'digitala')),
                 new html_table_cell(get_string('results_status', 'digitala')),
-                new html_table_cell(get_string('results_report', 'digitala')),
                 new html_table_cell(get_string('results_timestamp', 'digitala')),
+                new html_table_cell(get_string('results_report', 'digitala')),
                 new html_table_cell(add_delete_all_attempts_button()));
             foreach ($headers as $value) {
                 $value->header = true;
